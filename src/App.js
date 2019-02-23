cbc861f4ee8ab1b082b26dce9be9176f60089493
Ademola Afolabi <ademola.afolabi@icloud.com>
2019-02-23 13:22:47 +0000

    rebuild homepage with bulma

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'react-bulma-components/dist/react-bulma-components.min.css';
+import { Button, Columnns, Menu, Hero, Heading, Container, Section } from 'react-bulma-components';
+import { CalculatorContainer, CalculatorMenu } from './CalculatorContainer';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+     return <CalculatorContainer>
+            <Hero color="primary">
+              <Hero.Body>
+                <Container>
+                  <Heading>GDP Calculator</Heading>
+                  <Button>
+                     Learn More
+                  </Button>
+                </Container>
+              </Hero.Body>
+            </Hero>
+          <Section>
+                      
+          </Section>
+      </CalculatorContainer>
   }
 }
 
